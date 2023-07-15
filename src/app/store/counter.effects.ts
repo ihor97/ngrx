ca0cb59bd455024d93d0c0a4a3acbfd78c51b5d6
ihor97 <daniel.waffner@gmail.com>
2023-07-15 14:16:19 +0100

    383. The Old @Effect Decorator & Registering Effects

--- src/app/store/counter.effects.ts
+++ src/app/store/counter.effects.ts
@@ -1,16 +1,34 @@
-import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { Actions, createEffect, ofType} from "@ngrx/effects";
 import { decrement, increment } from "./counter.actions";
 import { tap } from "rxjs";
-export class CounterEffects{
+import { Injectable } from "@angular/core";
+// export class CounterEffects{
 
+//     constructor(private actions$:Actions){}
+//     // actions$ емітить нам значення коли в аппці action відправляється
+//     saveCount=createEffect(()=> this.actions$.pipe(
+//         ofType(increment,decrement ),
+//         tap((action)=>{
+//             console.log(action);
+//             localStorage.setItem('count',action.value.toString())
+//         })
+//         // мусимо поставити false щоб вказує на те що не буде відправлятися новий action 
+//     ),{dispatch:false})
+// }
+
+
+
+@Injectable()
+export class CounterEffects{
     constructor(private actions$:Actions){}
-    // actions$ емітить нам значення коли в аппці action відправляється
+// старий варіант
+    // @Effect({dispatch:false})
+    
     saveCount=createEffect(()=> this.actions$.pipe(
         ofType(increment,decrement ),
         tap((action)=>{
             console.log(action);
             localStorage.setItem('count',action.value.toString())
         })
-        // мусимо поставити false щоб вказує на те що не буде відправлятися новий action 
     ),{dispatch:false})
 }
