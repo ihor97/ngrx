a5ca8bee5329f8e1bc01a4453e388772b7449b2e
ihor97 <daniel.waffner@gmail.com>
2023-07-15 11:38:45 +0100

    376. Handling Actions Without createReducer

--- src/app/store/counter.actions.ts
+++ src/app/store/counter.actions.ts
@@ -1,4 +1,5 @@
 import { createAction, props } from "@ngrx/store";
+
 export const increment=createAction(
     '[Counter] increment',
     // дозволяє добавляти якісь дані до action
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -3,12 +3,17 @@ import { increment } from './counter.actions';
 
 const initialState = 0;
 
-export const counterReducer = createReducer(
-  initialState,
-  // в action попадає наша data з функції props
-  on(increment, (state,action)=>state+action.value)
-);
+// export const counterReducer = createReducer(
+//   initialState,
+//   // в action попадає наша data з функції props
+//   on(increment, (state,action)=>state+action.value)
+// );
 
-// export function counterReducer(state=initialState) {
-//   return initialState
-// }
+
+export function counterReducer(state=initialState,action:any) {
+  // action.type це є ключ в actions
+  if(action.type==='[Counter] increment'){
+    return state+action.value
+  }
+  return initialState
+}
