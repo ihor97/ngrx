70719d8682bb138255ec14baaa81a7a0b6e85217
ihor97 <118623656+ihor97@users.noreply.github.com>
2023-07-15 15:06:54 +0100

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { StoreModule } from '@ngrx/store';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    SharedModule,
+    CoreModule,
   ],
-  imports: [BrowserModule, StoreModule.forRoot({
-  })],
-  providers: [],
   bootstrap: [AppComponent],
+  // providers: [LoggingService]
 })
 export class AppModule {}
